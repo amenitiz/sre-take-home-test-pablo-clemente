9f2d85cb37b852cd4afb5456da3e7d6a0e8760a1
rodrpb <rodrpb>
2026-07-09 10:46:47 +0200

    test: restore service default assertion

--- status-page/test/models/service_test.rb
+++ status-page/test/models/service_test.rb
@@ -50,6 +50,6 @@ class ServiceTest < ActiveSupport::TestCase
 
   test "default status is operational" do
     service = Service.new(name: "Test", url: "https://example.com")
-    assert_equal "maintenance", service.status
+    assert_equal "operational", service.status
   end
 end
